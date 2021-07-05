200c054a43a955718b8d06b4d92924007b7eb47b
nthun <thomassocska@yahoo.com>
2021-07-05 11:53:00 +0200

    Change code to tidyverse Add dataset Add pilot analysis code

--- self_regulation_pilot.R
+++ self_regulation_pilot.R
@@ -1,21 +1,38 @@
 #mechanisms of self-regulation pilot study
 
-library(foreign)
-file.choose()
-main_dataset <- read.spss("C:\\Users\\theot\\Desktop\\project f\\pilot_study\\pilot_study_analysis\\main_dataset.sav",
-                          to.data.frame = TRUE)
-library(tidyr)
-library(dplyr)
-df_perc_corr_inhibitions_neutral <- main_dataset %>%
-  select(PPID1, Group, T1_perc_corr_inhibitions_neutral,
-         T3_perc_corr_inhibitions_neutral)
-df_perc_corr_inhibitions_neutral = tidyr::gather(df_perc_corr_inhibitions_neutral, key=time, value=score, 
-                                                 T1_perc_corr_inhibitions_neutral:T3_perc_corr_inhibitions_neutral) %>% arrange(PPID1)
-df_perc_corr_inhibitions_neutral$Group <- as.factor(df_perc_corr_inhibitions_neutral$Group) # Groups were integers, but turned to factors.
-
-library(lme4)
+library(tidyverse)
+library(haven)
 library(lmerTest)
-my_model <- lmerTest::lmer(score ~ time*Group + (1 | PPID1), data = df_perc_corr_inhibitions_neutral)
-
+library(sjPlot)
 library(effects)
-plot(allEffects(my_model))
+
+selfreg_raw <- read_spss("data/2021_04_24_project_F_pilot_data.sav")
+
+glimpse(selfreg_raw)
+
+selfreg_long <-
+  selfreg_raw %>%
+  select(PPID1,
+         group = Group,
+         T1_perc_corr_inhibitions_neutral,
+         T3_perc_corr_inhibitions_neutral) %>% 
+  pivot_longer(T1_perc_corr_inhibitions_neutral:T3_perc_corr_inhibitions_neutral, 
+               
+               names_to = "time",
+               values_to = "score") %>% 
+  arrange(PPID1) %>% 
+  mutate(time = str_remove(time, "_perc_corr_inhibitions_neutral") %>% 
+                as.factor(),
+         group = case_when(group == 1 ~ "tDCS",
+                           group == 2 ~ "EEG-Feedback",
+                           group == 3 ~ "UMC") %>% 
+                 fct_inorder())
+
+# Groups were integers, but turned to factors.
+
+pilot_model <- lmer(score ~ time * group + (1 | PPID1), 
+                    data = selfreg_long)
+
+tab_model(pilot_model, show.std = TRUE, show.est = FALSE)
+
+plot(allEffects(pilot_model))
